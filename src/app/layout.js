340f6161cfc0ffd659a388b34f707aaac0a22936
Aashis719 <aashishnep10@gmail.com>
2025-06-14 22:20:34 +0545

    change

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Inter, Playfair_Display } from 'next/font/google';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './globals.css';
-// This file is used to set up the global layout for the application,
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
