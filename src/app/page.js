6b102522cf9f17b1d92e7e4d333bc7d2ebfe65a4
Aashis719 <aashishnep10@gmail.com>
2025-06-15 22:45:44 +0545

    metadata

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { homeMetadata } from './metadata';
 import HomeContent from '@/app/components/HomeContent';
 
 export const metadata = homeMetadata;
-
+// This metadata will be used for the home page
 export default function HomePage() {
   return <HomeContent />;
 }
