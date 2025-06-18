35b17c9fbc23a1dae79fb285aad840b6d7769490
Aashis719 <aashishnep10@gmail.com>
2025-06-18 21:55:24 +0545

    importing gallery

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import GalleryComponent from './GalleryComponent'
 import { galleryMetadata } from '../metadata'
 export const metadata = galleryMetadata;
+// src/app/gallery/page.js
 const page = () => {
   return (
     <div>
