4e4a897bed7b8caefda855dcb957402cc1c03bfa
Aashis719 <aashishnep10@gmail.com>
2025-06-19 22:00:01 +0545

    imported lucide-react in footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,7 +2,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Instagram, Facebook, Twitter } from 'lucide-react';
-
+// Importing icons from lucide-react
+// Importing icons for social media links
 const Footer = () => {
   const footerSections = [
     {
