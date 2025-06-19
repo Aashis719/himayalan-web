c29f59d34790026b82c4370c1b77fadc31c97300
Aashis719 <aashishnep10@gmail.com>
2025-06-19 21:59:01 +0545

    main page changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@ import { homeMetadata } from './metadata';
 import HomeContent from '@/app/components/HomeContent';
 
 export const metadata = homeMetadata;
-
+// src/app/page.js
+// This file defines the metadata for the home page and imports the HomeContent component to display the
 export default function HomePage() {
   return <HomeContent />;
 }
