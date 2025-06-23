b0e319d00506d25bcda455761347d9ca21cb3460
Aashis719 <aashishnep10@gmail.com>
2025-06-23 22:20:19 +0545

    gallery component changed

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -2,8 +2,7 @@ import React from 'react'
 import GalleryComponent from './GalleryComponent'
 import { galleryMetadata } from '../metadata'
 export const metadata = galleryMetadata;
-// src/app/gallery/page.js
-// This file defines the metadata for the gallery page and imports the GalleryComponent to display the gallery.
+
 const page = () => {
   return (
     <div>
