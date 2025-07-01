cca87f8be9341ab5eb7d086393e120e2eaf67a04
Aashis719 <aashishnep10@gmail.com>
2025-07-01 21:04:32 +0545

    a better footer section

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Instagram, Facebook, Twitter } from 'lucide-react';
-
+// Importing social media icons from lucide-react
 const Footer = () => {
   const footerSections = [
     {
