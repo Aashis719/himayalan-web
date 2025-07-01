d35bec3ae8f89f4a85ec677bbdd02bd03c7355a9
Aashis719 <aashishnep10@gmail.com>
2025-07-01 21:03:09 +0545

    Look book updated

--- src/app/components/Lookbookhome.js
+++ src/app/components/Lookbookhome.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { motion, AnimatePresence } from 'framer-motion';
 import { X, ZoomIn } from 'lucide-react';
-
+// Importing icons from lucide-react for close and zoom functionality
 const Lookbookhome = () => {
   const [selectedImage, setSelectedImage] = useState(null);
 
