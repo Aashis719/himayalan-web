cd59c7aafa16ef11c1d939e37449f0460b63c95b
Aashis719 <aashishnep10@gmail.com>
2025-07-04 21:39:54 +0545

    Contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ContactComponent from './ContactComponent'
 import { contactMetadata } from '@/app/metadata'
 export const metadata = contactMetadata;
-// src/app/contact/page.js           
+         
 const page = () => {
   return (
     <div>
