f95648085d05bf3a30b48bd484c92470a5f8aafe
Aashish Neupane <162073333+Aashis719@users.noreply.github.com>
2025-09-11 22:01:55 +0545

    Update GalleryComponent.js

--- src/app/gallery/GalleryComponent.js
+++ src/app/gallery/GalleryComponent.js
@@ -8,7 +8,7 @@ export default function LookbookPage() {
   const lookbookImages = [  
     {
       src: '/images/lookbook/image1.webp', 
-      alt: 'Artisan weaving a traditional pattern',
+      alt: 'Artisan weaving a traditional pattern', 
       width: 'col-span-2',
     },
     {
